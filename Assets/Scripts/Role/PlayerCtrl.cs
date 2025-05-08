73afe6483fbc9a7546e3fc3567a1ab3b99a6e1f8
Soraku7 <2673140287@qq.com>
2025-05-08 20:17:30 +0800

    Init

---
+++ Assets/Scripts/Role/PlayerCtrl.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+
+    private Rigidbody2D rig;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
