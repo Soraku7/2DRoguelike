8fe4756184cd0122e4526f4de4481d4675d92976
Soraku7 <2673140287@qq.com>
2025-05-13 23:32:17 +0800

    Refactor PlayerCtrl and update SampleScene settings
    Revised `PlayerCtrl` to use `FixedUpdate` for movement, added serialized fields for joystick and move speed, and applied a `Rigidbody2D` requirement. Adjusted `SampleScene` with updated render and lighting settings, introduced new GameObjects with prefabs, and enhanced UI structure for future scalability.

--- Assets/Scripts/Role/PlayerCtrl.cs
+++ Assets/Scripts/Role/PlayerCtrl.cs
@@ -1,18 +1,26 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerCtrl : MonoBehaviour
 {
 
+    [Header("Elements")]
     private Rigidbody2D rig;
+    
+    [SerializeField] private MobileJoystick joystick;
 
+    [Header("Setting")]
+    [SerializeField] private float moveSpeed;
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
     }
 
-
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rig.linearVelocity = joystick.GetMoveVector() * (moveSpeed * Time.deltaTime);
+
     }
+
 }
