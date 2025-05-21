f4ac18d164d77d5802ec0a3bc4a101cb0b38b76d
Soraku7 <2673140287@qq.com>
2025-05-21 21:16:24 +0800

    Add AutoAim functionality to Weapon class
    Introduced an AutoAim feature that automatically adjusts the weapon's aim toward the closest enemy within range. Refactored existing logic to separate the calculation of the closest enemy and aim adjustment for better readability and maintainability.

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -6,22 +6,38 @@ public class Weapon : MonoBehaviour
     [Header("Settings")] 
     [SerializeField] private float range;
 
-    [SerializeField] private LayerMask enemyMask; 
+    [SerializeField] private LayerMask enemyMask;
+
+    [Header("Animation")] 
+    [SerializeField] private float aimLerp;
     
     private void Update()
     {
+        AutoAim();
+    }
+
+    private void AutoAim()
+    {
+        Enemy closestEnemy = GetClosestEnemy();
+        
+        Vector2 targetUpVector = Vector2.up;
+
+        if (closestEnemy != null) targetUpVector = (closestEnemy.transform.position - transform.position).normalized;
+        
+        transform.up = Vector3.Lerp(transform.up , targetUpVector , aimLerp * Time.deltaTime);
+    }
+
+    private Enemy GetClosestEnemy()
+    {
         Enemy closeEnemy = null;
+        Vector2 targetUpVector = Vector2.up;
 
         Collider2D[] enmies = Physics2D.OverlapCircleAll(transform.position, range, enemyMask);
         
 
         float minDistance = range;
 
-        if (enmies.Length == 0)
-        {
-            transform.up = Vector3.up;
-            return;
-        }
+        if (enmies.Length <= 0) return null;
         
         for (int i = 0; i < enmies.Length; i++)
         {
@@ -34,14 +50,8 @@ public class Weapon : MonoBehaviour
                 minDistance = distanceToEnemy;
             }
         }
-
-        if (closeEnemy == null)
-        {
-            transform.up = Vector3.up;
-            return;
-        }
         
-        transform.up = (closeEnemy.transform.position - transform.position).normalized;
+        return closeEnemy;
     }
 
     private void OnDrawGizmos()
