ff3664bfd4ee3a58206134ea6b7737988bba1bbb
Soraku7 <2673140287@qq.com>
2025-05-24 15:15:18 +0800

    Refactor damage text system to use object pooling.
    Replaced direct instantiation of damage text with an object pool to improve performance and memory management. Integrated pooling logic for spawning, reusing, and releasing objects. Updated callback implementation to support new pool-based workflow.

--- Assets/Scripts/Manager/DamageTextManager.cs
+++ Assets/Scripts/Manager/DamageTextManager.cs
@@ -1,27 +1,66 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
-using Random = UnityEngine.Random;
+using UnityEngine.Pool;
 
 public class DamageTextManager : MonoBehaviour
 {
     [Header("Elements")]
     [SerializeField] private DamageText damageTextPrefab;
+    
+    [Header("Pooling")]
+    private ObjectPool<DamageText> damageTextPool;
 
     private void Awake()
     {
-        Enemy.onDamageTaken += InstantiateDamageText;
+        Enemy.onDamageTaken += EnemyHitCallBack;
+    }
+
+    private void Start()
+    {
+        damageTextPool = new ObjectPool<DamageText>(CreateFunction , ActionOnGet , ActionOnRelease , ActionOnDestroy);
+    }
+
+    private DamageText CreateFunction()
+    {
+        return Instantiate(damageTextPrefab , transform);
+    }
+
+    private void ActionOnGet(DamageText damageText)
+    {
+        damageText.gameObject.SetActive(true);
+    }
+    
+    private void ActionOnRelease(DamageText damageText)
+    {
+        damageText.gameObject.SetActive(false);
+    }
+
+    private void ActionOnDestroy(DamageText damageText)
+    {
+        Destroy(damageText.gameObject);
     }
 
     private void OnDestroy()
     {
-        Enemy.onDamageTaken -= InstantiateDamageText;
+        Enemy.onDamageTaken -= EnemyHitCallBack;
     }
-
-    [NaughtyAttributes.Button]
-    private void InstantiateDamageText(int damage , Vector2 enemyPos)
+    
+    private void EnemyHitCallBack(int damage , Vector2 enemyPos)
     {
+        DamageText damageTextInstance = damageTextPool.Get();
+        
         Vector3 spawnPosition = enemyPos + Vector2.up * 1.5f;
-        DamageText damageTextInstance = Instantiate(damageTextPrefab ,spawnPosition , Quaternion.identity , transform);
+        damageTextInstance.transform.position = spawnPosition;
+        
         damageTextInstance.Animate(damage);
+
+        float timer = 0;
+
+        DOTween.To(() => timer, x => timer = x, 1, 1f).OnStepComplete(() =>
+            {
+                damageTextPool.Release(damageTextInstance);
+            });    
+
     }
 }
