425854f3cd285f942e6d229ca031a6b0fcd7880c
Soraku7 <2673140287@qq.com>
2025-06-09 20:55:02 +0800

    Add WaveManagerUI and enhance wave-based gameplay
    Implemented `WaveManagerUI` script for displaying wave and timer information. Integrated with `WaveManager` for real-time UI updates. Adjusted `RangeEnemy` prefab and added spawn positioning logic, improving enemy wave transitions and UI feedback. Updated `SampleScene` with new UI elements for enhanced gameplay experience.

--- Assets/Scripts/Manager/WaveManager.cs
+++ Assets/Scripts/Manager/WaveManager.cs
@@ -2,30 +2,72 @@ using System;
 using System.Collections.Generic;
 using NaughtyAttributes;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
+[RequireComponent(typeof(WaveManagerUI))]
 public class WaveManager : MonoBehaviour
 {
+    [Header("Elements")]
+    [SerializeField] private Player player;
+    private WaveManagerUI waveManagerUI;
+    
     [Header("Settings")]
     [SerializeField] private float waveDuration;
     private float timer;
+    private bool isTimeOn;
+    private int currentWaveIndex;
     
     [Header("Waves")]
     [SerializeField] private Wave[] waves;
     private List<float> localCounters = new List<float>();
 
+    private void Awake()
+    {
+        waveManagerUI = GetComponent<WaveManagerUI>();
+    }
+    
     private void Start()
     {
         localCounters.Add(1);
+
+        StartWave(currentWaveIndex);
+        waveManagerUI.UpdateTimerText("Wave" + (currentWaveIndex + 1) + " / " + waves.Length);
     }
-    
+
     private void Update()
     {
-        if (timer < waveDuration) ManageCurrentWave();
+        if(!isTimeOn) return;
+
+        if (timer < waveDuration)
+        {
+            ManageCurrentWave();
+
+            string timeString = ((int)(waveDuration - timer)).ToString();
+            waveManagerUI.UpdateWaveText(timeString);
+        }
+        else StartWaveTransition();
+    }
+
+    private void StartWaveTransition()
+    {
+        waveManagerUI.UpdateTimerText("Wave" + (currentWaveIndex + 1) + " / " + waves.Length);
+        
+        isTimeOn = false;
+        
+        currentWaveIndex++;
+        
+        if(currentWaveIndex >= waves.Length)
+        {
+            waveManagerUI.UpdateWaveText("Game Over");
+            
+            return;
+        }
+        StartWave(currentWaveIndex);
     }
 
     private void ManageCurrentWave()
     {
-        Wave currentWave = waves[0];
+        Wave currentWave = waves[currentWaveIndex];
 
         for (int i = 0; i < currentWave.segments.Count; i++)
         {
@@ -41,14 +83,48 @@ public class WaveManager : MonoBehaviour
             
             if (timeSinceSegementStart / spawnDelay > localCounters[i])
             {
-                Instantiate(currentSegment.prefab , Vector2.zero , Quaternion.identity , transform);
+                Instantiate(currentSegment.prefab , GetSpawnPosition() , Quaternion.identity , transform);
                 localCounters[i]++;
             } 
         }
         
         timer += Time.deltaTime;
     }
+
+    [NaughtyAttributes.Button]
+    private void DefeatAllEnemies()
+    {
+        transform.Clear();
+    }
+
+    private Vector2 GetSpawnPosition()
+    {
+        Vector2 direction = Random.onUnitSphere;
+        Vector2 offset = direction.normalized * Random.Range(6, 10);
+        Vector2 targetPosition = (Vector2)player.transform.position + offset;
+
+        targetPosition.x = Mathf.Clamp(targetPosition.x, -18, 18);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, -8, 8);
+        
+        return targetPosition;
+    }
+    
+    private void StartWave(int waveIndex)
+    {
+        Debug.Log("Start Wave" + currentWaveIndex);
+        
+        timer = 0;
+        isTimeOn = true;
+        
+        localCounters.Clear();
+        foreach (var waveSegment in waves[waveIndex].segments)
+        {
+            localCounters.Add(1);
+        }
+    }
+
 }
+
 
 [Serializable]
 public struct Wave
