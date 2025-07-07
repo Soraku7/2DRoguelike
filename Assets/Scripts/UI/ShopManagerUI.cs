5f2e06f42cc218dfc199fa8b965c75f6d92f79e6
Soraku7 <2673140287@qq.com>
2025-07-08 02:09:02 +0800

    Add inventory panel management to ShopManagerUI for improved UI functionality

--- Assets/Scripts/UI/ShopManagerUI.cs
+++ Assets/Scripts/UI/ShopManagerUI.cs
@@ -8,21 +8,33 @@ public class ShopManagerUI : MonoBehaviour
     [SerializeField] private RectTransform playerStatsPanel;
     [SerializeField] private GameObject playerStatsClosePanel;
 
-    private float width;
+    private float playerStatsWidth;
     
+    [Header("Inventory Elements")]
+    [SerializeField] private RectTransform inventoryPanel;
+    [SerializeField] private GameObject inventoryClosePanel;
     
+    private float inventoryWidth;
     IEnumerator Start()
     {
         yield return null;
         
         ConfigurePlayerStatsPanel();
+        ConfigureInventoryPanel();
+    }
+
+    private void ConfigureInventoryPanel()
+    {
+        inventoryWidth = Screen.width / (4f * inventoryPanel.lossyScale.x);
+        inventoryPanel.offsetMax = inventoryPanel.offsetMin.With(x: -inventoryWidth);
+        Debug.Log(inventoryPanel.offsetMin.With(x: -inventoryWidth));
+        HideInventory();
     }
 
     private void ConfigurePlayerStatsPanel()
     {
-        width = Screen.width / (4f * playerStatsPanel.lossyScale.x);
-        playerStatsPanel.offsetMax = playerStatsPanel.offsetMax.With(x: width);
-        
+        playerStatsWidth = Screen.width / (4f * playerStatsPanel.lossyScale.x);
+        playerStatsPanel.offsetMax = playerStatsPanel.offsetMax.With(x: playerStatsWidth);
         HidePlayerStats();
     }
 
@@ -31,7 +43,7 @@ public class ShopManagerUI : MonoBehaviour
         playerStatsClosePanel.SetActive(true);
 
         playerStatsPanel.DOKill();
-        playerStatsPanel.DOAnchorPos(Vector2.right * (width / 2), 0.5f);
+        playerStatsPanel.DOAnchorPos(Vector2.right * (playerStatsWidth / 2), 0.5f);
     }
     
     public void HidePlayerStats()
@@ -39,6 +51,22 @@ public class ShopManagerUI : MonoBehaviour
         playerStatsClosePanel.SetActive(false);
         
         playerStatsPanel.DOKill();
-        playerStatsPanel.DOAnchorPos(Vector2.left * (width / 2), 0.5f);
+        playerStatsPanel.DOAnchorPos(Vector2.left * (playerStatsWidth / 2), 0.5f);
+    }
+
+    public void ShowInventory()
+    {
+        inventoryClosePanel.SetActive(true);
+
+        inventoryPanel.DOKill();
+        inventoryPanel.DOAnchorPos(Vector2.left * (inventoryWidth / 2), 0.5f);
+    }
+
+    public void HideInventory()
+    {
+        inventoryClosePanel.SetActive(false);
+        
+        inventoryPanel.DOKill();
+        inventoryPanel.DOAnchorPos(Vector2.right * (inventoryWidth / 2), 0.5f);
     }
 }
